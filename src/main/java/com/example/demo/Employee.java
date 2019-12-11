f070281168fcd00b3e1c7b33cb4db8682b0744a5
khanhluong1 <derick.luong@gmail.com>
2019-12-11 10:05:12 -0500

    Exercise203

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private long SSN;
+    private String DOB;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(long SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
